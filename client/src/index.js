8d80f80c18d1e3d58de2958f134da2815d8aeb7d
OnShubham <shubhamkharche3005@gmail.com>
2024-07-08 22:31:13 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -11,18 +11,7 @@ root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
+    
     </React.StrictMode>
   </BrowserRouter>
 );
