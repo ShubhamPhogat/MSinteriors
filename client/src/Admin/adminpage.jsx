8b997bc3ae1f3a3891c0a33c3d598ee435d61505
OnShubham <shubhamkharche3005@gmail.com>
2024-07-09 10:10:26 +0530

    logout fun add

--- client/src/Admin/adminpage.jsx
+++ client/src/Admin/adminpage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "../styles/admin.css"
 
 const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      <p>Welcome to the admin dashboard.</p>
+    <div className="admin-container">
+      <div className="admin-inner-container">
+        <h1 className="admin-title">Admin Dashboard</h1>
+        <p className="admin-welcome">Welcome to the admin dashboard.</p>
+      </div>
     </div>
   );
 };
